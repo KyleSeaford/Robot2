c25930f0197568ed0dad8d0eef427aefc08f508b
Kyle Seaford <147045805+KyleSeaford@users.noreply.github.com>
2024-03-06 13:09:31 +0000

    Add multithreading support for motor control

--- code/moters_bumpers.py
+++ code/moters_bumpers.py
@@ -1,7 +1,12 @@
 from evdev import InputDevice, list_devices, categorize, ecodes, KeyEvent
 import time
+from threading import Thread
 
 import RPi.GPIO as GPIO
+
+
+# Global variable to control motor state
+running = False
 
 def setUP():
     # Set up the GPIO pins
@@ -29,51 +34,61 @@ GPIO.setup(LED_PIN_A, GPIO.OUT)
 LED_PIN_B = 21
 GPIO.setup(LED_PIN_B, GPIO.OUT)
 
-def rightFORWARD(sec):
+def rightFORWARD():
+    global running
     setUP()
-    # Right motor backward
+    # Right motor forward
     GPIO.output(17, False)
     GPIO.output(22, True)
-    # Left motor forward
-    GPIO.output(23, True) 
+    # Left motor off
+    GPIO.output(23, False) 
     GPIO.output(24, False)
 
-    time.sleep(sec)
-    GPIO.cleanup()
+    while running:
+        pass  # Keep running until 'running' is False
+
+
 
 def rightBACK(sec):
     setUP()
-    GPIO.output(17, False)
-    GPIO.output(22, True)
-    # all motors backwards
+    # Right motor backward
+    GPIO.output(17, True)
+    GPIO.output(22, False)
+    # Left motor off
     GPIO.output(23, False) 
-    GPIO.output(24, True)
+    GPIO.output(24, False)
 
-    time.sleep(sec)
-    GPIO.cleanup()
+    while running:
+        pass  # Keep running until 'running' is False
+
+
 
 def leftFORWARD(sec):
     setUP()
-    # Right motor forward
-    GPIO.output(17, True)
+    # Right motor off
+    GPIO.output(17, False)
     GPIO.output(22, False)
     # Left motor backward
     GPIO.output(23, False) 
     GPIO.output(24, True)
 
-    time.sleep(sec)
-    GPIO.cleanup()
+    while running:
+        pass  # Keep running until 'running' is False
+
+
 
 def leftBACK(sec):
     setUP()
-    GPIO.output(17, True)
+    # Right motor off
+    GPIO.output(17, False)
     GPIO.output(22, False)
-    # all motors backwards
+    # left motors backwards
     GPIO.output(23, True) 
     GPIO.output(24, False)
 
-    time.sleep(sec)
-    GPIO.cleanup()
+    while running:
+        pass  # Keep running until 'running' is False
+
 
 def Start_delay():
     # delay while the controller is turned on
@@ -136,16 +151,25 @@ for event in device.read_loop():
         #    event.timestamp(), event.code, keyevent.keycode[0], 'down' if event.value else 'up'))  
         
         if keyevent.keycode[0] == 'BTN_LB':
-            leftFORWARD(2)
+            if keyevent.state == 1:  # Button press
+                running = True
+                Thread(target=leftFORWARD).start()  # Start motor in a new thread
+            elif keyevent.state == 0:  # Button release
+                running = False  # Stop the motor
+
 
         elif keyevent.keycode[0] == 'BTN_RB':
-            rightFORWARD(2)
+            if keyevent.state == 1:  # Button press
+                running = True
+                Thread(target=rightFORWARD).start()  # Start motor in a new thread
+            elif keyevent.state == 0:  # Button release
+                running = False  # Stop the motor
             
-        
 
         # SHUT DOWN EVERYTHING, XBOX button    
         elif keyevent.keycode[0] == 'K':
             GPIO.output(LED_PIN_B, GPIO.HIGH)
             time.sleep(1)
             GPIO.output(LED_PIN_B, GPIO.LOW)
+            GPIO.cleanup()
             break
